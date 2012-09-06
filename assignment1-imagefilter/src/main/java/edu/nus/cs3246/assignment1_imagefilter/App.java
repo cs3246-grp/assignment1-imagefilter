82e28032f18af82fc2be7fdc0d2ccc4216e57e04
Gao Yingkai <popol1991@gmail.com>
2012-09-06 11:25:50 +0800

    original structure

---
+++ assignment1-imagefilter/src/main/java/edu/nus/cs3246/assignment1_imagefilter/App.java
@@ -0,0 +1,13 @@
+package edu.nus.cs3246.assignment1_imagefilter;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
