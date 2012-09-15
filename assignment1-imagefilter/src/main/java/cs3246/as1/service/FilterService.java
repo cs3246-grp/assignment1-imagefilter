6a8076bc32799c9befc337098c87405b6ea085d4
Gao Yingkai <popol1991@gmail.com>
2012-09-15 11:49:45 +0800

    webapp all

---
+++ assignment1-imagefilter/src/main/java/cs3246/as1/service/FilterService.java
@@ -0,0 +1,10 @@
+package cs3246.as1.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class FilterService {
+	public String message() {
+		return "I'm filter service";
+	}
+}
